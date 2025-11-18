86469de55073606139b318cb853087ed94174e47
kaede <c0a24205f3@edu.teu.ac.jp>
2025-11-18 17:11:24 +0900

    練習9:背景のループ

--- pygame_image.py
+++ pygame_image.py
@@ -18,9 +18,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = tmr % 3200 
         screen.blit(bg_img, [-x, 0]) #練習5
         screen.blit(bg_img2, [-x + 1600, 0]) #練習7
+        screen.blit(bg_img, [-x + 3200, 0]) #練習9
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1
