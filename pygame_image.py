cb9634630df6141544cc452781d0e4b3637f5c8a
kaede <c0a24205f3@edu.teu.ac.jp>
2025-11-18 16:41:01 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -17,10 +17,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(10)
 
 
