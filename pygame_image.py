fcedaaf34e33c33a6b1a0ca6ba69f4fc59a25b8f
kaede <c0a24205f3@edu.teu.ac.jp>
2025-11-18 17:30:37 +0900

    課題1:こうかとん自然に動く

--- pygame_image.py
+++ pygame_image.py
@@ -29,6 +29,7 @@ def main():
         screen.blit(bg_img, [-x + 3200, 0]) #練習9
         
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
@@ -36,7 +37,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
         
         screen.blit(kk_img, kk_rct) #練習4
               
