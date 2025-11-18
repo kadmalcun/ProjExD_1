1e6d3b72ef6708ceb3fd13374539e686c7d318f2
kaede <c0a24205f3@edu.teu.ac.jp>
2025-11-18 16:52:10 +0900

    練習8:背景を連続にする

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False) #練習8
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False) #練習3
     tmr = 0
@@ -19,7 +20,7 @@ def main():
 
         x = tmr
         screen.blit(bg_img, [-x, 0]) #練習5
-        screen.blit(bg_img, [-x + 1600, 0]) #練習7
+        screen.blit(bg_img2, [-x + 1600, 0]) #練習7
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1
